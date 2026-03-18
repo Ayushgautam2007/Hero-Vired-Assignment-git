8c911f8f8ec23f3a2722276694cc8ad7275d2b4d
Ayushgautam2007 <159703362+Ayushgautam2007@users.noreply.github.com>
2026-03-18 15:17:53 +0530

    Update geometry_calculator.py

--- geometry_calculator.py
+++ geometry_calculator.py
@@ -12,10 +12,6 @@ if __name__ == "__main__":
 
     calculator = GeometryCalculator()
 
-    # TODO: Implement the feature to calculate the area of a circle
-
-    # TODO: Implement the feature to calculate the area of a rectangle
-
 
 radius = 5
 print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
